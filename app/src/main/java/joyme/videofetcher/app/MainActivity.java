fd526866b5638725cd1fe3c9800fd896474486dd
sumakira <skyzhang.mel@gmail.com>
2014-10-28 15:24:29 +0800

    Loading m3u8 file for original SOHU urls successfully

--- app/src/main/java/joyme/videofetcher/app/MainActivity.java
+++ app/src/main/java/joyme/videofetcher/app/MainActivity.java
@@ -1,11 +1,11 @@
 package joyme.videofetcher.app;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
@@ -22,19 +22,33 @@ import static java.lang.System.out;
 
 public class MainActivity extends Activity {
 
+    private final int HTML_LOADED = 0;
+    private final int JS_GENERATED = 1;
+    private final int JS_LOADED = 2;
+    private int mCurrentStage = HTML_LOADED;
     private WebView mWebview;
     private Button mButton;
-    //    private String mUrl = "http://hot.vrs.sohu.com/ipad1006750_4625602509181_4217071.m3u8?plat=h5";
-    private String mUrl = "http://pad.tv.sohu.com/20130415/n372763498.shtml";
+//    private String mUrl = "http://hot.vrs.sohu.com/ipad1006750_4625602509181_4217071.m3u8?plat=h5";
+        private String mUrl = "http://pad.tv.sohu.com/20130415/n372763498.shtml";
+//    private String mLexTvUrl = "http://m.letv.com/ptv/vplay/21036600.html";
     protected String mHtmlText = "";
     protected MainActivity mSelf;
-    private static int count = 0;
+    private int count = 0;
     protected View rootview;
     protected boolean flag = false;
 
     private Timer mTimer;
     private TimerTask mTimertask;
     private Handler mHandler;
+    private Message mMsg;
+
+    private final static String FUNC = "var doc = document.getElementsByTagName('head')[0];var script = document.createElement('script');script.type = 'text/javascript';var funstr = \"function myFunction() {var htmstr = document.getElementsByTagName('body')[0].innerHTML; if (htmstr.substr(0,6) == 'jsonp1'){htmstr = htmstr.replace('jsonp1', '');htmstr = htmstr.replace('\"; funstr+='\"highVid\":'; funstr+=\"', '<video ><source src=');\";funstr+=\"htmstr =htmstr.replace('\";funstr+=',\"ip\":';funstr+=\"', '/></video>');\";funstr+=\"document.getElementsByTagName('body')[0].innerHTML = htmstr; }else{ var h = ''+(new Date).getTime();\";funstr+=\"var sig=h._shift_en([23, 12, 131, 1321]);\";var sohuUrl = 'http://pad.tv.sohu.com/playinfo?callback=jsonp1';funstr+=\"sohuUrl +='&sig='+ sig;\";funstr+=\"sohuUrl += '&vid=' + vid;\";funstr+=\"var vidstr = ''+vid;\";funstr+=\"sohuUrl += '&key=' + vidstr._shift_en([23, 12, 131, 1321]);\";funstr+=\"sohuUrl +='&playlistid=' + playlistId;\";funstr+=\"window.location.href=sohuUrl;}\";funstr+=\"}\"; script.text =  funstr;doc.appendChild(script);";
+
+    private final static String JS_FUNC = "myFunction();";
+
+    //    private final static String LEXTV_JSFUNC = "$('#j-player').trigger('click')";
+//    private final static String LEXTV_JSFUNC = "javascript:$('.hv_ico_pasued').trigger('click')";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +58,24 @@ public class MainActivity extends Activity {
         rootview = ((ViewGroup) this.findViewById(android.R.id.content)).getChildAt(0);
 
         mSelf = this;
+        mMsg = new Message();
         mTimer = new Timer();
+
         mWebview = (WebView) findViewById(R.id.main_webview);
         mButton = (Button) findViewById(R.id.main_btn);
 
         mButton.setVisibility(View.GONE);
         mWebview.getSettings().setJavaScriptEnabled(true);
         mWebview.getSettings().setUserAgentString("Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53");
+//        mWebview.getSettings().setUserAgentString("Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53");
         mWebview.addJavascriptInterface(new HtmlLoader(), "loader");
-
+        mWebview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         mWebview.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                mWebview.loadUrl("javascript:window.loader.loadHtml('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+                super.onPageFinished(view, url);
+                mMsg.what = mCurrentStage;
+                mHandler.sendMessage(mMsg);
             }
 
             @Override
@@ -68,32 +87,45 @@ public class MainActivity extends Activity {
 
         mWebview.loadUrl(mUrl);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
         mHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
+
                 switch (msg.what) {
-                    case 1:
-                        out.println("checking html determined by timer");
-//                        mWebview.loadUrl(mUrl);
+                    case HTML_LOADED:
+                        mWebview.loadUrl("javascript:" + FUNC);
+                        out.println("==========    " + "stage one " + "   ===========");
+                        break;
+
+                    case JS_GENERATED:
+                        mWebview.loadUrl("javascript:myFunction();");
+//                        mWebview.loadUrl("javascript:window.loader.loadHtml('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+                        out.println("==========    " + " stage two" + "   ===========");
+                        break;
+
+                    case JS_LOADED:
                         mWebview.loadUrl("javascript:window.loader.loadHtml('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+                        out.println("==========    " + "stage three " + "   ===========");
                         break;
                 }
-            }
-        };
 
-        mTimertask = new TimerTask() {
-            @Override
-            public void run() {
-                Message msg = new Message();
-                msg.what = 1;
-                out.println("====== sending msg======");
-                mHandler.sendMessage(msg);
+                mCurrentStage++;
+
+                if (mTimertask == null || mTimertask.cancel()) {
+                    mTimertask = new TimerTask() {
+                        @Override
+                        public void run() {
+                            mMsg = new Message();
+                            mMsg.what = mCurrentStage;
+                            mHandler.sendMessage(mMsg);
+                        }
+                    };
+                    mTimer.schedule(mTimertask, 3000);
+                } else {
+                    out.println("==========    " + " timertaske cancelled failed " + "   ===========");
+                }
+
+                out.println("==========    " + mCurrentStage + "   ===========");
             }
         };
     }
@@ -103,32 +135,6 @@ public class MainActivity extends Activity {
         public void loadHtml(final String str) {
             mHtmlText = str;
             out.print("str is " + mHtmlText);
-
-            if (mHtmlText.contains("sig")) {
-                Toast.makeText(mSelf, "Params captured in " + count + " loading times", Toast.LENGTH_LONG).show();
-                out.println("================== " + Thread.currentThread().getStackTrace()[2].getClassName() + " ===============");
-                out.println(" Params captured in " + count + " loading times");
-
-                mTimer.cancel();
-            } else {
-                out.println("================== " + Thread.currentThread().getStackTrace()[2].getClassName() + " ===========load again ====");
-                out.println("");
-                mWebview.loadUrl(mUrl);
-                mTimer.schedule(mTimertask, 1000);
-                count++;
-
-//                mWebview.reload();
-            }
-
-            if (flag) {
-                return;
-            }
         }
     }
-
-    void reloadWebview() {
-        mWebview.reload();
-    }
-
-
 }
